8ca63801c077d717a23817a07d38fdc4ced112f0
Vishal Pandey <vishalpandey10022000@gmail.com>
2024-03-09 12:55:13 +0530

    changed something

--- website-sample/src/app/app.module.ts
+++ website-sample/src/app/app.module.ts
@@ -2,13 +2,56 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Routes ,RouterModule} from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
 
+let routes:Routes=[
+  {
+    path:'',
+    redirectTo:"home",
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutUsComponent
+  },
+  {
+    path:'services',
+    component: ServicesComponent
+  },
+  {
+    path:'contact',
+    component:ContactUsComponent
+  },
+  {
+    path:'**',
+    component:ErrorComponent
+  }
+  
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ServicesComponent,
+    NavbarComponent,
+    FooterComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
